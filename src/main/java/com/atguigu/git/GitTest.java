994704d3b23616d2ad91cc82e9ad3133a9cb311d
snn <snn@qq.com>
2022-06-08 11:53:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author snn
+ * @create 2022-06-08-11:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
